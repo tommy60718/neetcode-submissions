574578689547f21fd870bdc72dcd7998f64fab2e
tommy60718 <128281234+tommy60718@users.noreply.github.com>
2026-09-21 10:00:07 +0900

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Q1: How to record visited?
+        # Q2: every value is distinct? If not, how to trace?
+
+        # Use Floyd's tortoise and Hare method
+        # 1. Core: If cycle exists, the hare will finally catch up on tortoise
+        # 2. Case study: 
+            # Case: If the cycle lenth 10, than the Hare will catch up on tortoise 
+                # in 10 steps at most: 
+            # Math: The distance between Hare and tortoise = 10
+                # Each step : 10 + 1(tortoise) -2 (Hare) -> each step they get 1 closer
+            # Answer: at most 10 steps the Hare will catch up on Tortoise.
+        fast, slow = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
